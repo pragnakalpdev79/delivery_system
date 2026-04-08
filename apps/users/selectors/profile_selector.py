b0f8eea62ffa6d0fb953730999afb490e6207aac
pragnakalpdev79 <pragnakalp.dev79@gmail.com>
2026-04-08 18:56:11 +0530

    08/04/2026

--- apps/users/selectors/profile_selector.py
+++
@@ -1,6 +0,0 @@
-from apps.users.models import CustomerProfile
-
-class ProfileSelector:
-    @staticmethod
-    def get_user_profile(ruser):
-        return CustomerProfile.objects.select_related('user').get(user=ruser)
